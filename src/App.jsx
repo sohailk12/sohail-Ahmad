03ef4fbff4d3c6ecc65436c76492e5a135a4aaf8
Sohail Ahmad <sa492284@gmail.com>
2024-08-23 18:34:16 +0500

    control overflow in sm

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import Certifications from './components/Certifications'
 import { Navbar } from './components/Navbar'
 
 const App = () =>{
-  return (
-    <>
+  return <div className='overflow-x-hidden'>
       <Navbar />
       <Hero />
       <Experience/>
@@ -16,8 +15,7 @@ const App = () =>{
       <Certifications/>
       <Contact/>
       <div className="fixed inset-0 -z-10 h-full w-full bg-sky-200 bg-[linear-gradient(to_right,#b5e4ff_1px,transparent_1px),linear-gradient(to_bottom,#b5e4ff_1px,transparent_1px)]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_60%_450px,#fff,transparent)]"></div></div>
-    </>
-  )
+      </div>
 }
 
 export default App
